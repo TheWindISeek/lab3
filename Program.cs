09cbc54af650c83c7564a08818da41f56b3ad7c7
JeffreySharp <FAIZ030612@163.com>
2023-03-09 10:05:58 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cycle
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            // 输入一个正整数 逆序打印出每一个位数
+            // 编程找出1000以内所有的完数 一个完数所有的因子的和刚好是完数
+            //对输入的一组数据进行排序
+            Application.Run(new Form1());
+        }
+    }
+}
